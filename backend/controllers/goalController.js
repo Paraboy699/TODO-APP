7ab54dd27934e1f51b74e0edc789caf843cadb9a
Pukar Singh <spukar133@gmail.com>
2024-03-28 13:06:09 +0545

    Routes , Controller and MIddleware Setup

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,18 +1,24 @@
-const getGoals = (req, res) => {
+const asyncHandler = require("express-async-handler");
+
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get goals" });
-};
+});
 
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Pleaser add a text field");
+  }
   res.status(200).json({ message: "Set Goals" });
-};
+});
 
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update goal ${req.params.id}` });
-};
+});
 
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete goal  ${req.params.id}` });
-};
+});
 
 module.exports = {
   getGoals,
